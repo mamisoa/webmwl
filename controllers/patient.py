05c3d9ff398dcc9b13a4ae88698d8ab1c259dbae
Koteswararao Gummadidala -X (kogummad - VARITE INDIA PVT LTD at Cisco) <kogummad@cisco.com>
2018-06-11 21:27:38 +0530

    bug with patient_id is fixed

--- controllers/patient.py
+++ controllers/patient.py
@@ -1,35 +1,27 @@
-# -*- coding: utf-8 -*-
-# -------------------------------------------------------------------------
-# This is a sample controller
-# this file is released under public domain and you can use without limitations
-# -------------------------------------------------------------------------
-
-# ---- example index page ----
 
 Items_Per_Page = 10
-from gluon.tools import Crud
 
 def index():
-	sort = 0
-	page = request.args(0, cast=int, default = 1)
-	stop = page*Items_Per_Page
-	start = stop - Items_Per_Page
-	req_sort = request.args(1, cast=int, default = 0)
+	redirect(URL('listpatient'))
 
-	total_entries = len(db(db.patient).select())
-	if total_entries % Items_Per_Page > 0:
-		total_entries = (total_entries / Items_Per_Page) + 1
-	else:
-		total_entries = (total_entries / Items_Per_Page)
+def cal_age(born):
+	from datetime import datetime
+	age = (datetime.now().date() - born).days / 365 
+	age_str = str(age) + ' years'
+	if age < 1:
+		age = (datetime.now().date() - born).days / 30
+		age_str = str(age) + ' months'
+		if age < 1:
+			age = (datetime.now().date() - born).days
+			age_str = str(age) + ' days'
+			if age < 1:
+				age_str = "Invalid age"
 
-	if req_sort == 0:
-		rows = db(db.patient).select(orderby=db.patient.created_on,limitby=(start,stop))
-	else:
-		rows = db(db.patient).select(orderby=~db.patient.created_on,limitby=(start,stop))
-	return locals()
+	return age_str
 
 def listpatient():
 	sort = 0
+	Items_Per_Page = 10
 	page = request.args(0, cast=int, default = 1)
 	stop = page*Items_Per_Page
 	start = stop - Items_Per_Page
@@ -45,22 +37,16 @@ def listpatient():
 		rows = db(db.patient).select(orderby=db.patient.created_on,limitby=(start,stop))
 	else:
 		rows = db(db.patient).select(orderby=~db.patient.created_on,limitby=(start,stop))
-	return locals()
+	search_str = request.vars.search_str
 
-def list_patient_by_datetime():
-	page = request.args(0, cast=int, default = 0)
-	start = page*Items_Per_Page
-	stop = start + Items_Per_Page
-	rows = db(db.patient).select(orderby=~db.patient.created_on,limitby=(start,stop))
-	return locals()
-
-def list_patients_by_name():
-	page = request.args(0, cast=int, default = 0)
-	start = page*Items_Per_Page
-	stop = start + Items_Per_Page
-	search_str = request.args(1, default="hello")
-	rows = db(db.patient.first_name.like('%search_str%', case_sensitive=False)).select(orderby=~db.patient.created_on,limitby=(start,stop))
-	#rows = db(db.patient).select(orderby=~db.patient.created_on,limitby=(start,stop))
+	if request.vars.search_str:
+		search_str = request.vars.search_str
+		rows = db(db.patient.first_name.contains(search_str) | 
+			db.patient.last_name.contains(search_str)).select(orderby=~db.patient.created_on,limitby=(start,stop))
+	else:
+		rows = db(db.patient).select(orderby=db.patient.created_on,limitby=(start,stop))
+	for row in rows:
+		row.age = cal_age(row.birth_date)
 	return locals()
 
 def addpatient():
@@ -70,10 +56,12 @@ def addpatient():
 	gender = request.vars.gender
 	weight = request.vars.weight
 	patient_size = request.vars.patient_size
+	patient_id = request.vars.patient_id
 	if len(request.vars)>0:
 		db.patient.insert(first_name=first_name, last_name=last_name,
 					birth_date=birth_date, gender=gender,
-					patient_size=patient_size, weight=weight)
+					patient_size=patient_size, weight=weight, 
+					patient_id = patient_id)
 		redirect(URL('listpatient'))
 	return locals()
 
@@ -96,6 +84,7 @@ def editpatient():
 			last_name=request.vars.last_name,
 			patient_size=request.vars.patient_size,
 			weight=request.vars.weight,
-			birth_date=request.vars.birth_date)
+			birth_date=request.vars.birth_date,
+			patient_id = request.vars.patient_id)
 		redirect(URL('listpatient'))
 		return locals()
